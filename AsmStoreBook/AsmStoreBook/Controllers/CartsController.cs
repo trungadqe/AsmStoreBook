c6d207ce3de54a067a5a2bb2b47a9e72ed5a8a16
trungadqe <86902641+trungadqe@users.noreply.github.com>
2022-05-02 10:27:50 +0700

    update
    update

--- AsmStoreBook/AsmStoreBook/Controllers/CartsController.cs
+++ AsmStoreBook/AsmStoreBook/Controllers/CartsController.cs
@@ -63,6 +63,52 @@ namespace AsmStoreBook.Controllers
             }
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> Checkout()
+        {
+            string thisUserId = _userManager.GetUserId(HttpContext.User);
+            List<Cart> myDetailsInCart = await _context.Cart
+                .Where(c => c.UId == thisUserId)
+                .Include(c => c.Book)
+                .ToListAsync();
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    //Step 1: create an order
+                    Order myOrder = new Order();
+                    myOrder.UId = thisUserId;
+                    myOrder.OrderDate = DateTime.Now;
+                    myOrder.Total = myDetailsInCart.Select(c => c.Book.Price)
+                        .Aggregate((c1, c2) => c1 + c2);
+                    _context.Add(myOrder);
+                    await _context.SaveChangesAsync();
+
+                    //Step 2: insert all order details by var "myDetailsInCart"
+                    foreach (var item in myDetailsInCart)
+                    {
+                        OrderDetail detail = new OrderDetail()
+                        {
+                            OrderId = myOrder.Id,
+                            BookIsbn = item.BookIsbn,
+                            Quantity = 1
+                        };
+                        _context.Add(detail);
+                    }
+                    await _context.SaveChangesAsync();
+
+                    //Step 3: empty/delete the cart we just done for thisUser
+                    _context.Cart.RemoveRange(myDetailsInCart);
+                    await _context.SaveChangesAsync();
+                    transaction.Commit();
+                }
+                catch (DbUpdateException ex)
+                {
+                    transaction.Rollback();
+                    Console.WriteLine("Error occurred in Checkout" + ex);
+                }
+            }
+            return RedirectToAction("Index", "Cart");
+        }
 
 
         // POST: Carts/Create
